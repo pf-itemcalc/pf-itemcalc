666de7da5b7670a53a1d2fe3d1d8f6769efdf0a1
Jason Pattle <jasonpattle90@gmail.com>
2025-07-03 15:38:05 +0100

    chore:  updated about

--- src/components/title/About.tsx
+++ src/components/title/About.tsx
@@ -37,7 +37,6 @@ const About = () => {
       <Typography>
         <b>This tool currently cannot:</b>
         <ul>
-          <li>Customise spell list and caster level (TODO)</li>
           <li>Calculate ammunition</li>
           <li>Calculate double-headed weapons (treated only single headed)</li>
           <li>Allow you to apply any metamagic feats to spells</li>
